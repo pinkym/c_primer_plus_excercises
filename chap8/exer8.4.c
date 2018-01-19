dc4f4d6fc1f069c85e7d42fc9598e87c108b368b
node-mail <1320381667@qq.com>
2018-01-19 14:50:38 +0800

    Update exer8.4.c

--- chap8/exer8.4.c
+++ chap8/exer8.4.c
@@ -5,28 +5,29 @@
 	如何统计单词个数？dhjskh
 	连续的读入字符，如果遇到空格，则一个单词结束。
 	一个问题是，必须在单独的一行中输入^D，系统才会识别EOF
+	windows系统则不同，只要在一行任意位置输入^C都会识别EOF
 */
 
 int main(){
-        char ch;
-        int word_count=0;
-        double char_count=0;
+	char ch;
+	int word_count=0;
+	double char_count=0;
 
-        printf("please enter a string, enter EOF to end.\n");
-        while((ch = getchar()) != EOF){
-			if(ch == ' ' || ch == '\n'){
-					continue;
-			}else{
-				word_count++;
-				while(ch != ' ' && ch != '\n'){
-						char_count++;
-						ch = getchar();
-				}
+	printf("please enter a string, enter EOF to end.\n");
+	while((ch = getchar()) != EOF){
+		if(ch == ' ' || ch == '\n'){
+				continue;
+		}else{
+			word_count++;
+			while(ch != ' ' && ch != '\n'){
+				char_count++;
+				ch = getchar();
 			}
-        }
+		}
+	}
 
-        double avg = (word_count == 0)? 0: (char_count/word_count);
-        printf("you have entered %d words, %lf chars,\n the average char is %lf\n", word_count, char_count, avg);
-        printf("bye!");
-        return 0;
+	double avg = (word_count == 0)? 0: (char_count/word_count);
+	printf("you have entered %d words, %lf chars,\n the average char is %lf\n", word_count, char_count, avg);
+	printf("bye!");
+	return 0;
 }
